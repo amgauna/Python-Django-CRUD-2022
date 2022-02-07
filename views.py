f1b0076245eac6e5ab96ba942a601dccecee89c6
Ana Gauna <amgauna@gmail.com>
2022-02-07 15:28:40 -0300

    Update views.py

--- views.py
+++ views.py
@@ -1,10 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from app.forms import CarrosForm
 from app.models import Carros
 
+def form(request):
+    return render(request, 'form.html')
+
+# Create your views here.
+def home(request):
+    return HttpResponse('Hello World')
+
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+# Podemos também passar comandos de dados vindos do backend para o 
+# frontend utilizando {%%} e impressões literais usando {{}}
+
+{% if carro == 'Fiat Uno' %}
+    Esse é um carro {{carro}}
+{% else %}
+    Não tem carro
+{% endif %}
 
 def form(request):
     data = {}
