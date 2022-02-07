6bcb7ba5d5b1b43598f0d0c7acc8b3acb0149553
Ana Gauna <amgauna@gmail.com>
2022-02-07 16:04:35 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -20,3 +20,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static') [
 'PORT': '3306',
 'OPTIONS': { 'init_command': 'SET default_storage_engine=InnoDB', }
 ]
+
+# O código acima traz o tipo do banco://usuario:senha@host/nome_do_banco
+# Esses dados deverão ser substituidos em project/settings:
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
