e0e3b8352492607f6cd1c03cc0e8315bff0dc829
Ana Gauna <amgauna@gmail.com>
2022-02-07 13:23:45 -0300

    Update app_views.py

--- app_views.py
+++ app_views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+def form(request):
+    return render(request, 'form.html')
 
 # Create your views here.
 def home(request):
     return HttpResponse('Hello World')
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
