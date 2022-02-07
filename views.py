1552d7591a7f0f01370f5fb0a04fb423f2c1e424
Ana Gauna <amgauna@gmail.com>
2022-02-07 13:42:05 -0300

    Update views.py

--- views.py
+++ views.py
@@ -23,3 +23,17 @@ def view(request, pk):
     return render(request, 'view.html', data)
 
 
+<div class="col-8 m-auto pt-2 pb-2 text-center">
+    <h1>Visualização</h1>
+</div>
+
+<div class="col-8 m-auto pt-3 pb-2 text-center">
+    <a href="/" class="btn btn-info">Voltar</a>
+</div>
+
+<div class="col-8 m-auto pt-4 pb-2 text-center">
+    <strong>Modelo: </strong>{{db.modelo}}<br>
+    <strong>Marca: </strong>{{db.marca}}<br>
+    <strong>Ano: </strong>{{db.ano}}<br>
+</div>
+
