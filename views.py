5e30a1f9f59f7c4c6c0c330cf139af975f2d2101
Ana Gauna <amgauna@gmail.com>
2022-02-07 15:29:46 -0300

    Update views.py

--- views.py
+++ views.py
@@ -40,6 +40,21 @@ def view(request, pk):
     return render(request, 'view.html', data)
 
 
+def edit(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    data['form'] = CarrosForm(instance=data['db'])
+    return render(request, 'form.html', data)
+
+def update(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    form = CarrosForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+
 <div class="col-8 m-auto pt-2 pb-2 text-center">
     <h1>Visualização</h1>
 </div>
