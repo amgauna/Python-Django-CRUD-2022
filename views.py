92a543b22dbe7ce53a50c1d55fd300cda99973f4
Ana Gauna <amgauna@gmail.com>
2022-02-07 13:34:08 -0300

    Update views.py

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from app.forms import CarrosForm
+from app.models import Carros
 
 # Create your views here.
 def home(request):
@@ -9,3 +10,16 @@ def form(request):
     data = {}
     data['form'] = CarrosForm()
     return render(request, 'form.html', data)
+
+def create(request):
+    form = CarrosForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+def view(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    return render(request, 'view.html', data)
+
+
