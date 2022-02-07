2a1e246b302c4738771df2cecdaf55f3b352da9e
Ana Gauna <amgauna@gmail.com>
2022-02-07 15:39:48 -0300

    Update views.py

--- views.py
+++ views.py
@@ -69,3 +69,20 @@ def update(request, pk):
     <strong>Ano: </strong>{{db.ano}}<br>
 </div>
 
+# Apagar dados no Python com Ajax
+# Vamos criar a url que será responsável por eliminar os dados no nosso banco:
+
+from django.contrib import admin
+from django.urls import path
+from app.views import delete
+
+urlpatterns = [
+    path('delete/<int:pk>/', delete, name='delete'),
+]
+
+# Já vamos criar a função view relativa ao delete:
+
+def delete(request, pk):
+    db = Carros.objects.get(pk=pk)
+    db.delete()
+    return redirect('home')
