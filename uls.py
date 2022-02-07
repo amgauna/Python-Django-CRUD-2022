6e289bfad64b093205c9ebd78d0064f93ba705d3
Ana Gauna <amgauna@gmail.com>
2022-02-07 13:27:02 -0300

    Update uls.py

--- uls.py
+++ uls.py
@@ -1,6 +1,10 @@
-from app.views import home
+from django.contrib import admin
+from django.urls import path
+from app.views import home, form
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('form/', form),
 ]
+
