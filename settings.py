a88fda03b9f0afea32ee210031cf46a53f6b8df6
Ana Gauna <amgauna@gmail.com>
2022-02-07 15:54:32 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,6 +6,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app'
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    
+
+# Dentro do arquivo de configurações precisamos setar o novo banco de dados:
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') [
+'ENGINE': 'django.db.backends.mysql',
+'NAME': "testa",
+'HOST': 'localhost',
+'USER': 'root',
+'PASSWORD': '',
+'PORT': '3306',
+'OPTIONS': { 'init_command': 'SET default_storage_engine=InnoDB', }
+]
